2f327efb3cc663bb9e6b9a45a1b39fb2b00adf91
Linetes <linetes@github.com>
2018-01-30 02:36:01 -0600

    90% terminado

--- MyWebPage/JavaScript/lab6.js
+++ MyWebPage/JavaScript/lab6.js
@@ -10,3 +10,19 @@ function myFunction3() {
 function myFunction4() {
     document.getElementById("but").style.color = 'blue';
 }
+function myFunction5() {
+    alert('Hello');
+}
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
