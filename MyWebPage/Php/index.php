0ec7a2c3923d50001f522bd1ac7d3e15a64f8265
Linetes <linetes@github.com>
2018-02-06 14:26:50 -0600

    Empezando php

---
+++ MyWebPage/Php/index.php
@@ -0,0 +1,9 @@
+<?php
+include("../Partials/_imports");
+include("../Partials/_header.html");
+$saludo = "Hola mundo!";
+echo "<h1>$saludo</h1>";
+?>    
+
+<?=$saludo?>
+<?php include("../Partials/_footer.html");
