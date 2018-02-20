4368e4eefb6563df5895eb5a31a227d49c220a73
Linetes <A01207499@itesm.mx>
2018-02-19 21:33:40 -0600

    Agregado lab 10

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-include("Partials/_imports.html");
-include("Partials/_header.html");
+include("partials/_imports.html");
+include("partials/_header.html");
 include("Html/index.html");
 $saludo = "Hola mundo!";
 echo "<h1>$saludo</h1>";
 ?>
 
 <?=$saludo?>
-<?php include("Partials/_footer.html");
+<?php include("partials/_footer.html");
