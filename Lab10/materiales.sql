a5620d697deb26a87ddcbe39d8a17a55c86319be
Linetes <A01207499@itesm.mx>
2018-02-22 23:13:50 -0600

    borrado inecesario

--- Lab10/materiales.sql
+++
@@ -1,8 +0,0 @@
-BULK INSERT a1207499.a1207499.[Materiales]
-   FROM 'e:\wwwroot\a1207499\materiales.csv'
-   WITH
-      (
-         CODEPAGE = 'ACP',
-         FIELDTERMINATOR = ',',
-         ROWTERMINATOR = '\n'
-      )
--- Lab10/proveedores.sql
+++
@@ -1,8 +0,0 @@
-BULK INSERT a1207499.a1207499.[Proveedores]
-   FROM 'e:\wwwroot\a1207499\proveedores.csv'
-   WITH
-      (
-         CODEPAGE = 'ACP',
-         FIELDTERMINATOR = ',',
-         ROWTERMINATOR = '\n'
-      )
