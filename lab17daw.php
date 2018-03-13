ab3734dddacce2d374e8728e5b5c7396f8bc81bc
Linetes <A01207499@itesm.mx>
2018-03-12 23:54:23 -0600

    Lab 17-19 Con Preguntas y js, html y php de ajax

--- lab17daw.php
+++ lab17daw.php
@@ -28,7 +28,7 @@ include("partials/_header.html");
                   <div class="card bg-light mb-3" style="max-width: 18rem;">
                     <div class="card-header">¿Qué importancia tiene AJAX en el desarrollo de RIA's (Rich Internet Applications?</div>
                     <div class="card-body">
-                        <p></p>
+                        <p>AJAX hace las funciones sin necesidad de hacer reload a la página, esto le da un valor estra a la página ya que envuelve más al usuario.</p>
                     </div>
                   </div>
                 </div>
@@ -37,6 +37,7 @@ include("partials/_header.html");
                     <div class="card-header">¿Qué implicaciones de seguridad tiene AJAX? ¿Dónde se deben hacer las validaciones de seguridad, del lado del cliente o del lado del servidor?</div>
                     <div class="card-body">
                       <p>
+                          AJAX, al igual que JS es muy debil en términos de seguridad, por ello se hace del lado del servidor el AJAX para que el servidor pueda detener funciones maliciosas.
                       </p>
                     </div>
                   </div>
@@ -45,7 +46,11 @@ include("partials/_header.html");
                       <div class="card bg-light mb-3" style="max-width: 18rem;">
                           <div class="card-header">¿Qué es JSON?</div>
                           <div class="card-body">
-                              <p></p>
+                              <p>
+                                  JavaScript Object Notation: Es un formato para intercambiar datos de forma muy poco pesada, es muy parecida la sintaxis a la familia de C.
+                                  <br>
+                                  Guarda datos en Strings para poder usarlos.
+                              </p>
                           </div>
                       </div>
                   </div>
@@ -54,7 +59,9 @@ include("partials/_header.html");
           <div class="jumbotron jumbotron-fluid">
               <div class="container">
                 <h1 class="display-4">Bibliografía:</h1>
-                <p class="lead">NA .(2018). JSON. Obtenido en: http://json.org</p>
+                  <p class="lead">NA .(2018). JSON. Obtenido en: http://json.org</p>
+                  <p class="lead">NA .(2018). Intro to JSON. W3Schools. Obtenido en: https://www.w3schools.com/js/js_json_intro.asp</p>
+                  <p class="lead">Chaikin Hall .(2018). Rich Internet Applications with Ajax. Johns Hopkins. Obtenido en: https://ep.jhu.edu/programs-and-courses/605.787-rich-internet-applications-with-ajax</p>
               </div>
           </div>
         </div>
