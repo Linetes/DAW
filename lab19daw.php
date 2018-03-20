0c83911b21904e655920ee15ce3b40faa7fd3602
Linetes <A01207499@itesm.mx>
2018-03-20 00:02:09 -0600

    Borrado cosas inecesarias

--- lab19daw.php
+++ lab19daw.php
@@ -20,42 +20,7 @@ include("partials/_header.html");
     <div class="container">
         <h1 class="display-4"></h1>
         <div class="alert alert-light" role="alert">
-            <style>
-                * {
-                    box-sizing: border-box;
-                }
 
-                #myInput {
-                    background-image: url('/css/searchicon.png');
-                    background-position: 10px 10px;
-                    background-repeat: no-repeat;
-                    width: 100%;
-                    font-size: 16px;
-                    padding: 12px 20px 12px 40px;
-                    border: 1px solid #ddd;
-                    margin-bottom: 12px;
-                }
-
-                #myTable {
-                    border-collapse: collapse;
-                    width: 100%;
-                    border: 1px solid #ddd;
-                    font-size: 18px;
-                }
-
-                #myTable th, #myTable td {
-                    text-align: left;
-                    padding: 12px;
-                }
-
-                #myTable tr {
-                    border-bottom: 1px solid #ddd;
-                }
-
-                #myTable tr.header, #myTable tr:hover {
-                    background-color: #f1f1f1;
-                }
-            </style>
             <h1>Por Nombre</h1>
             <form action="lab19daw.php" method="POST">
                 <label for="usuario">Fruta que quieres</label>
@@ -78,26 +43,6 @@ include("partials/_header.html");
                 <?php getFruits();?>
                 </tbody>
             </table>
-
-            <script>
-                function myFunction() {
-                    var input, filter, table, tr, td, i;
-                    input = document.getElementById("myInput");
-                    filter = input.value.toUpperCase();
-                    table = document.getElementById("myTable");
-                    tr = table.getElementsByTagName("tr");
-                    for (i = 0; i < tr.length; i++) {
-                        td = tr[i].getElementsByTagName("td")[0];
-                        if (td) {
-                            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                                tr[i].style.display = "";
-                            } else {
-                                tr[i].style.display = "none";
-                            }
-                        }
-                    }
-                }
-            </script>
         </div>
     </div>
 </div>
